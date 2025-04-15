6454e0e6ad53950c32b1830317a2bc8ec1f825a6
Akanksha Kushwaha <151656066+akankshakusf@users.noreply.github.com>
2025-04-14 22:07:38 -0400

    Add files via upload

---
+++ 5.Agents that code with Tools/MY-P02-S01-07-Agents_that_code_skills.py
@@ -0,0 +1,138 @@
+import os 
+from dotenv import load_dotenv
+from autogen.code_utils import create_virtual_env
+from autogen.coding import CodeBlock,LocalCommandLineCodeExecutor
+from autogen import ConversableAgent,AssistantAgent
+import datetime
+import yfinance
+import pandas 
+import matplotlib.pyplot as plt  
+
+# import environment variables
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+# llm configuration
+llm_config = {
+    "model":"gpt-4o-mini",
+    "api_key":api_key
+}
+
+# First skill: Get stock prices
+def get_stock_prices(stock_symbols, start_date, end_date):
+    """Get the stock prices for the given stock symbols between
+    the start and end dates.
+
+    Args:
+        stock_symbols (str or list): The stock symbols to get the
+        prices for.
+        start_date (str): The start date in the format 
+        'YYYY-MM-DD'.
+        end_date (str): The end date in the format 'YYYY-MM-DD'.
+    
+    Returns:
+        pandas.DataFrame: The stock prices for the given stock
+        symbols indexed by date, with one column per stock 
+        symbol.
+    """
+    
+
+    stock_data = yfinance.download(
+        stock_symbols,start=start_date, end= end_date
+    )
+    return stock_data.get("Close")
+
+
+# Second Skill: plot the stocks 
+def plot_stock_prices(stock_prices, filename):
+    """Plot the stock prices for the given stock symbols.
+
+    Args:
+        stock_prices (pandas.DataFrame): The stock prices for the 
+        given stock symbols.
+    """
+    
+
+    plt.figure(figsize=(10,5))
+    for column in stock_prices.columns:
+        plt.plot(stock_prices.index, stock_prices[column],label=column)
+
+    plt.title("Stock Prices")
+    plt.ylabel("Price")
+    plt.xlabel("Date")
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(filename)
+
+# command line executor
+
+venv_dir = "env_llm"
+venv_context = create_virtual_env(venv_dir)
+
+executor = LocalCommandLineCodeExecutor(
+    virtual_env_context=venv_context,
+    timeout=200,
+    work_dir="coding",
+    functions=[get_stock_prices,plot_stock_prices],
+)
+
+print(executor.execute_code_blocks(code_blocks=[CodeBlock(language="python", code = "import sys;print(sys.executable)")]))
+
+
+# Agent 1: Code Writer agent
+code_writer_agent = AssistantAgent(
+    name="code_writer_agent",
+    llm_config=llm_config,
+    code_execution_config =False,
+    human_input_mode="NEVER"
+) 
+
+#check system prompt message 
+code_writer_agent_system_message = code_writer_agent.system_message
+# And we have to let llm know through its prompts of their existence
+# The executor will automatically generate a prompt for all our functions as long as they're properly documented:
+print(executor.format_functions_for_prompt()) 
+
+# So we can add this to the code writer agent's prompt
+code_writer_agent_system_message +=executor.format_functions_for_prompt()
+# The complete prompt now contains additional information about our used defined functions
+print(code_writer_agent_system_message)
+
+# import pdb; pdb.set_trace()
+
+# Now I will update the code_writer_agent prompt because it has these tools 
+code_writer_agent = ConversableAgent(
+    name="code_writer_agent",
+    system_message=code_writer_agent_system_message,
+    llm_config=llm_config,
+    code_execution_config=False,
+    human_input_mode="NEVER",
+)
+
+# Agent 2:Code Executor Agent
+code_executor_agent = ConversableAgent(
+    name="code_executor_agent",
+    llm_config=llm_config,
+    code_execution_config={"executor":executor},
+    human_input_mode="ALWAYS",
+    default_auto_reply="Please continue.If eveything is done, reply 'TERMINATE'.",
+)
+
+# find current datetime
+today=datetime.datetime.now().date()
+
+#instantiate the chat 
+message = f"Today is {today}. "\
+"Create a plot showing the normalized price of NVDA and BTC-USD for the last 5 years "\
+"with their 60 weeks moving average. "\
+"Make sure the code is in markdown code block, print the normalized prices, save the figure"\
+" to a file asset_analysis.png and show it. Provide all the code necessary in a single python bloc. "\
+"Re-provide the code block that needs to be executed with each of your messages. "\
+"If python packages are necessary to execute the code, provide a markdown "\
+"sh block with only the command necessary to install them and no comments."
+
+# Let's define the chat and initiate it !
+chat_result = code_executor_agent.initiate_chat(
+    code_writer_agent,
+    message=message
+)
